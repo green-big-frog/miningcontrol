90e1a689ca9194e7bd1be0fcc1ff5ca4a3c9ed8b
Jonathan <greenbigfrog@gmail.com>
2016-05-30 15:11:58 +0200

    We now got a PoC!

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # miningcontrol
+
+To get this working copy `example.config.json` to `config.json` and then edit it.
+
+Setting a value to 0, will prevent that algo from getting mined.
---
+++ main.rb
@@ -0,0 +1,49 @@
+require 'httparty'
+require 'json'
+require 'pry'
+require 'colorize'
+
+file = File.read('config.json')
+config = JSON.parse(file)
+
+address = config['address']
+
+while 1 > 0 do
+  url = 'https://www.nicehash.com/api?method=simplemultialgo.info'
+  response = HTTParty.get(url)
+  response = JSON.parse(response)
+
+  algo = 0
+  port = 0
+
+  previous_algo = algo
+  previous_port = port
+
+  result = response["result"]["simplemultialgo"]
+
+  earnings = []
+
+  result.each do |x|
+    paying = x['paying'].to_f
+    hash = config[x['name']]
+    earnings[x['algo']] = paying * hash
+  end
+
+  earnings.compact!
+
+  puts earnings.index(earnings.max)
+
+  result.each do |x|
+    if x['algo'] == earnings.index(earnings.max)
+      algo = x['name']
+      port = x['port']
+    end
+  end
+
+  unless previous_algo == algo
+    system 'killall ccminer'
+    system "ccminer -a #{algo} --url stratum+tcp://#{algo}.eu.nicehash.com:#{port} -u #{address}.miningcontrol -p x"
+  end
+
+  sleep(60)
+end
